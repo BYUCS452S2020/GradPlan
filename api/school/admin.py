9a4520cabffd94e5ee2d5826a42c28030a1a2de5
Brian Carter <brian152@byu.edu>
2020-05-12 17:55:19 -0600

    registered models with Django Admin

--- api/school/admin.py
+++ api/school/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from school.models import Course, Group, Major, School
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Group)
+admin.site.register(Major)
+admin.site.register(School)
