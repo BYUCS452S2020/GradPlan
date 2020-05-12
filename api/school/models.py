0792927c64ddf7fb24b6ce56fa08653d049e356c
Brian Carter <brian152@byu.edu>
2020-05-12 16:24:40 -0600

    Created school app with models for Schools, Majors, Groups, and Courses

---
+++ api/school/models.py
@@ -0,0 +1,31 @@
+from uuid import uuid4
+from django.db import models
+
+class School(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True, unique=True)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=20)
+
+class Major(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True, unique=True)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+class Group(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True, unique=True)
+    major = models.ForeignKey(Major, on_delete=models.CASCADE)
+    complete = models.IntegerField()
+
+class Course(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True, unique=True)
+    group = models.ManyToManyField(Group, through='CourseGroup')
+    department = models.CharField(max_length=100)
+    course_number = models.IntegerField()
+    credits = models.DecimalField(decimal_places=1, max_digits=2)
+    countsAsGeneral = models.BooleanField()
+
+class CourseGroup(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True, unique=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
