34574a70ba91abeeae250cff838cefc402b9f54f
Brian Carter <brian152@byu.edu>
2020-05-17 14:36:30 -0600

    Created basic CRUD endpoints for school models

--- api/school/admin.py
+++ api/school/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from school.models import Course, Group, Major, School
+from school.models import Course, CourseGroup, Group, Major, School
 
 admin.site.register(Course)
+admin.site.register(CourseGroup)
 admin.site.register(Group)
 admin.site.register(Major)
 admin.site.register(School)
---
+++ api/school/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework import routers
+from school import views
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+    path('schools/', views.SchoolList.as_view()),
+    path('schools/<uuid:school_id>/', views.SchoolDetail.as_view()),
+    path('schools/<uuid:school_id>/majors/', views.MajorList.as_view()),
+
+    path('major/<uuid:major_id>/', views.MajorDetail.as_view()),
+    path('major/<uuid:major_id>/groups/', views.GroupList.as_view()),
+
+    path('group/<uuid:group_id>/', views.GroupDetail.as_view()),
+    path('group/<uuid:group_id>/courses/', views.GroupCoursesList.as_view()),
+
+    path('course/<uuid:course_id>/', views.CourseDetail.as_view()),
+]
