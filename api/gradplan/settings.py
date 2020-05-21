9329f6989d66ad797855f87cb14af46fe37f15a8
Brian Carter <brian152@byu.edu>
2020-05-20 19:33:52 -0600

    Implemented Authentication in API using DRF Token Auth

--- api/gradplan/settings.py
+++ api/gradplan/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'school',
     'student',
 ]
@@ -130,3 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Django Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- api/student/views.py
+++ api/student/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from student.models import Student
+from student.serializers import StudentSerializer
 
-# Create your views here.
+class Login(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'student': StudentSerializer(user).data,
+            'token': token[0].key,
+        })
+
+class Register(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            student = Student.objects.create_user(
+                serializer.validated_data['email'],
+                request.data['password'],
+                first_name=serializer.validated_data['first_name'],
+                last_name=serializer.validated_data['last_name'])
+
+            return Response({
+                'student': StudentSerializer(student).data,
+                'token': student.auth_token.key
+            })
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
