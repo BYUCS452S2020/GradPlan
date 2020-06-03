7b3f69301d6844471a75b6bf56ee0511e4fac384
Brian Carter <brian152@byu.edu>
2020-06-02 19:05:55 -0600

    Fixed Model issue with MTM fields, and added delete endpoints for planned and completed courses

--- api/student/urls.py
+++ api/student/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('login', views.Login.as_view()),
     path('register', views.Register.as_view()),
     path('planned', views.PlannedCourses.as_view()),
+    path('planned/<uuid:course_id>', views.PlannedCourseDetail.as_view()),
     path('completed', views.CompletedCourses.as_view()),
+    path('completed/<uuid:course_id>', views.CompletedCourseDetail.as_view()),
 ]
